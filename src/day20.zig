71cf8d537c007e5cb9ae4d091f7f0cafa4c634cf
Stefan Lienhard <sl@stefanlienhard.ch>
2021-12-20 14:05:49 +0100

    day 20 part 2

--- src/day20.zig
+++ src/day20.zig
@@ -3,7 +3,10 @@ const assert = std.debug.assert;
 
 const data = @embedFile("../data/day20_input");
 
-fn printGrid(grid: *[106][106]u1) void {
+const num_iters: usize = 50;
+const dim = 100 + (num_iters + 1) * 2;
+
+fn printGrid(grid: *[dim][dim]u1) void {
     for (grid) |row| {
         for (row) |c| {
             if (c == 1) {
@@ -16,7 +19,7 @@ fn printGrid(grid: *[106][106]u1) void {
     }
 }
 
-fn enhance(algorithm: *std.StaticBitSet(512), grid: *[106][106]u1, x: usize, y: usize) u1 {
+fn enhance(algorithm: *std.StaticBitSet(512), grid: *[dim][dim]u1, x: usize, y: usize) u1 {
     const bits = [_]u1{
         grid[y + 1][x + 1],
         grid[y + 1][x + 0],
@@ -46,12 +49,13 @@ pub fn main() anyerror!void {
         }
     }
 
-    var grid = [_][106][106]u1{[_][106]u1{[_]u1{0} ** 106} ** 106} ** 2;
+    var grid = [_][dim][dim]u1{[_][dim]u1{[_]u1{0} ** dim} ** dim} ** 2;
     {
-        var y: usize = 3;
+        const fill_offset = num_iters + 1;
+        var y: usize = fill_offset;
         while (line_it.next()) |line| : (y += 1) {
             for (line) |c, i| {
-                const x = i + 3;
+                const x = i + fill_offset;
                 grid[0][y][x] = if (c == '#') 1 else 0;
             }
         }
@@ -70,10 +74,9 @@ pub fn main() anyerror!void {
         printGrid(grid[0]);
     }
 
-    const num_iters: usize = 2;
     var it: usize = 0;
     var out_of_range_value = grid[0][0][0];
-    while (it < num_iters) : (it += 1) {
+    while (it < num_iters) {
         const offset = num_iters - it;
         const grid_from = &grid[it % 2];
         const grid_to = &grid[(it + 1) % 2];
@@ -84,9 +87,7 @@ pub fn main() anyerror!void {
 
         for (grid_from) |row, y| {
             for (row) |_, x| {
-                if (x < offset or x >= (106 - offset) or
-                    y < offset or y >= (106 - offset))
-                {
+                if (x < offset or x >= (dim - offset) or y < offset or y >= (dim - offset)) {
                     grid_to[y][x] = out_of_range_value;
                 } else {
                     grid_to[y][x] = enhance(&algorithm, grid_from, x, y);
@@ -95,21 +96,26 @@ pub fn main() anyerror!void {
         }
 
         // printGrid(grid_to);
-    }
 
-    const num_lit = blk: {
-        var res: usize = 0;
-        const g = &grid[num_iters % 2];
-        for (g) |row| {
-            for (row) |val| {
-                if (val == 1) {
-                    res += 1;
+        it += 1;
+        if (it == 2 or it == 50) {
+            const num_lit = blk: {
+                var res: usize = 0;
+                const g = &grid[num_iters % 2];
+                for (g) |row| {
+                    for (row) |val| {
+                        if (val == 1) {
+                            res += 1;
+                        }
+                    }
                 }
+                break :blk res;
+            };
+            if (it == 2) {
+                std.debug.print("Day 20, part 1: num lit pixels after 2 steps = {}\n", .{num_lit});
+            } else if (it == 50) {
+                std.debug.print("Day 20, part 2: num lit pixels after 50 steps = {}\n", .{num_lit});
             }
         }
-        break :blk res;
-    };
-
-    std.debug.print("Day 20, part 1: num lit pixels = {}\n", .{num_lit});
-    std.debug.print("Day 20, part 2:\n", .{});
+    }
 }
