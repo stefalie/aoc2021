a6061988b8e18d4dc98e09ee03f51fbb7ec2d51a
Stefan Lienhard <sl@stefanlienhard.ch>
2021-12-25 01:02:52 +0100

    day 24 phew

--- src/day24.zig
+++ src/day24.zig
@@ -1,3 +1,5 @@
+// Most code in this file is not used.
+
 const std = @import("std");
 const assert = std.debug.assert;
 
@@ -132,6 +134,64 @@ fn parseProgram(src: []const u8, instructions: *std.ArrayList(Inst)) !void {
     }
 }
 
+// Unused
+fn zUpdateRoutine(z: isize, w: i8, params: [3]i8) isize {
+    if (@rem(z, 26) + params[1] != w) {
+        return @divTrunc(z, params[0]) * 26 + w + params[2];
+    } else {
+        return @divTrunc(z, params[0]);
+    }
+}
+
+fn recurse(params: [][3]i8, z: usize, number: usize, down: bool) ?usize {
+    if (params.len == 0) {
+        return if (z == 0) number else null;
+    }
+
+    const param0 = params[0][0];
+    const param1 = params[0][1];
+    const param2 = params[0][2];
+
+    // w = next_input();
+    // x = z % 26 + param1 != w
+    // z = z / param0 * (25 * x + 1) + (w + param2) * x
+
+    // param0 and param2 are always > 0
+    // param0 is either 1 or 26
+    // if param0 == 1, param1 >= 10 (just form lookings at params)
+    // therefore, x can only be 1 if param0 == 26
+    //
+    // If x == 1 a number is pushes onto a base26 stack
+    // If x == 0 we pop
+    // x can only be 1 if param0 == 26
+    // There are the same number of param0 == 26 and == 1. Therefore
+    // in order to reach z == 0 in the end, every param == 26 must
+    // be a pop. For this we have to pick w so that x == 0.
+
+    // Pop
+    if (param0 == 26) {
+        const pop_w = @intCast(i8, @rem(z, 26)) + param1;
+        if (pop_w < 1 or pop_w > 9) {
+            return null;
+        }
+        const new_z = @divTrunc(z, 26);
+        return recurse(params[1..], new_z, number * 10 + @intCast(u8, pop_w), down);
+    }
+
+    // Push
+    std.debug.assert(param0 == 1);
+    const range_down = [_]u8{ 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+    const range_up = [_]u8{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    for (if (down) range_down else range_up) |w| {
+        const new_z = z * 26 + w + @intCast(u8, param2);
+        if (recurse(params[1..], new_z, number * 10 + w, down)) |result| {
+            return result;
+        }
+    }
+
+    return null;
+}
+
 pub fn main() anyerror!void {
     var instructions = std.ArrayList(Inst).init(gpa);
     defer instructions.deinit();
@@ -142,9 +202,48 @@ pub fn main() anyerror!void {
     //for (instructions.items) |inst| {
     //    std.debug.print("{}\n", .{inst});
     //}
+    //
+    //
 
-    std.debug.print("Day 24, part 1:\n", .{});
-    std.debug.print("Day 24, part 2:\n", .{});
+    // Argh, I spent all the time building the interpreter above.
+    // I thought I could somehow find a lower bound by randomly or regularly
+    // sampling the input space. But it seems really hard to even find 1
+    // possible solution.
+    // Instead I should have looked at the input assembly. It seems to repeat
+    // 14 times the following routine and only modify 3 parameters:
+    // w = next_input();
+    // x = z % 26 + param1 != w
+    // z = z / param0 * (25 * x + 1) + (w + param2) * x
+    // Only the z register matters, everything else are just temp values.
+    // See also:
+    // https://www.reddit.com/r/adventofcode/comments/rnejv5/comment/hpsr7bz/?utm_source=share&utm_medium=web2x&context=3
+    // https://www.reddit.com/r/adventofcode/comments/rnejv5/comment/hpsk9nf/?utm_source=share&utm_medium=web2x&context=3
+    // https://www.reddit.com/r/adventofcode/comments/rnejv5/comment/hps5hgw/?utm_source=share&utm_medium=web2x&context=3
+
+    var params: [14][3]i8 = undefined;
+
+    var line_it = std.mem.tokenize(u8, data, "\r\n");
+    var routine_repeat_idx: usize = 0;
+    while (routine_repeat_idx < 14) : (routine_repeat_idx += 1) {
+        var i: usize = 0;
+        while (i < 4) : (i += 1) {
+            _ = line_it.next().?;
+        }
+        params[routine_repeat_idx][0] = (try parseLine(line_it.next().?)).rhs.?.val;
+        params[routine_repeat_idx][1] = (try parseLine(line_it.next().?)).rhs.?.val;
+        i += 2;
+        while (i < 15) : (i += 1) {
+            _ = line_it.next().?;
+        }
+        params[routine_repeat_idx][2] = (try parseLine(line_it.next().?)).rhs.?.val;
+        i += 1;
+        while (i < 18) : (i += 1) {
+            _ = line_it.next().?;
+        }
+    }
+
+    std.debug.print("Day 24, part 1: max model number = {}\n", .{recurse(params[0..], 0, 0, true)});
+    std.debug.print("Day 24, part 2: min model number = {}\n", .{recurse(params[0..], 0, 0, false)});
 }
 
 test "negate" {
